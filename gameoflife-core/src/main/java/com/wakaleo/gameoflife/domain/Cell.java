de1d6af2c6115d62471d2bfa510be2c5c74c82aa
Steve Deguir <sdeguirmendjian@globanet.com>
2018-10-21 13:50:46 -0400

    More comments

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
  /**
- * Wakaleo Consulting - John Ferguson Smart
+ * Wakaleo Consulting - John Ferguson Smart more comment 
  * Game of life, demonstration application for Jenkins: The Definitive Guide
  *
  * Cell.java
